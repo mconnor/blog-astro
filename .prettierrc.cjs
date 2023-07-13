eb63f46eaf9fb457832daf4b600cd2e4f3870665
mike connor <mike@mikeconnor.tech>
2023-07-13 13:31:17 -0500

    prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,15 +1,18 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  // i am just using the standard config, change if you need something else
-  ...require('prettier-config-standard'),
-  pluginSearchDirs: [__dirname],
-  plugins: [require.resolve('prettier-plugin-astro')],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro'
-      }
-    }
-  ]
+/** @type {import("prettier").Options} */
+const config = {
+    plugins: [require.resolve('prettier-plugin-astro')],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
 }
+
+module.exports = config
